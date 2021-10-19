d73f941d6eacf438e2d50b2cae3442251825168a
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2021-10-19 13:19:11 +0200

    Improve ConvertTest.test_convert1()

--- tests/convert.py
+++ tests/convert.py
@@ -1,6 +1,8 @@
 from ad_examples.aad.aad_globals import get_aad_option_list, AAD_IFOREST, AadOpts
 from ad_examples.aad.aad_support import get_aad_model
 from aad2onnx import convert, to_onnx
+import onnx
+import onnxruntime as rt
 import numpy as np
 import unittest
 
@@ -29,10 +31,22 @@ class ConvertTest(unittest.TestCase):
 
 		return model
 
+	def evaluate_onnx(self, o, data):
+		sess = rt.InferenceSession(o.SerializeToString())
+		input_name = sess.get_inputs()[0].name
+		label_name = 'score'
+		scores_onnx = sess.run([label_name], {input_name: data.astype(np.float32)})[0].reshape(-1)
+
+		return scores_onnx
+
 	def test_convert1(self):
 		x = np.random.normal(size=(20, 4)).astype(np.float32)
 
 		model = self.createAADModel(x)
+		scores_model = model.get_score(model.transform_to_ensemble_features(x)).astype(np.float32)
 
 		o = to_onnx(model, x)
-		pass
+		onnx.checker.check_model(o)
+		scores_onnx = self.evaluate_onnx(o, x)
+
+		np.testing.assert_allclose(scores_onnx, scores_model, 1e-5)
