d75e04159aeadbbebee00ed6f51879369539cd94
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2021-10-19 12:29:42 +0200

    Improve get_maximum_opset_supported()

--- aad2onnx/__init__.py
+++ aad2onnx/__init__.py
@@ -1,1 +1,4 @@
 from .convert import convert, to_onnx
+
+
+__max_supported_opset__ = 15
--- aad2onnx/proto.py
+++ aad2onnx/proto.py
@@ -1,4 +1,6 @@
 from onnx import defs
+from onnxconverter_common import onnx_ex
 
 def get_maximum_opset_supported():
-	return defs.onnx_opset_version()
+	from . import __max_supported_opset__
+	return min(__max_supported_opset__, onnx_ex.get_maximum_opset_supported())
