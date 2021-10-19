ef947bd815c92831344ecfaf685704a174bd1f48
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2021-10-19 20:07:22 +0300

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ setup(name=name,
 	packages=find_packages(exclude=(test_suite,)),
 	test_suite=test_suite,
 	license='MIT',
+	install_requires=[
+		'onnx',
+		'onnxconverter-common >= 1.8.0',
+		'onnxruntime',
+		'skl2onnx',
+		'ad_examples @ git+https://github.com/shubhomoydas/ad_examples.git@0a7b86c4f2f7306ff543a15b387fe938f9c06130#egg=ad_examples-0.0.1'
+	],
 	classifiers=[
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3',
